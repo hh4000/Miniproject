b43871579bd065cd0c282db2347881ad90c26643
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2021-11-11 14:22:17 +0100

    Fixed distCal function

--- XDrive.ino
+++ XDrive.ino
@@ -8,7 +8,7 @@ Zumo32U4Encoders encoders;
 Zumo32U4Motors motors;
 
 
-double xdist = 30;
+double xdist = 10;
 double ydist = 10;
 double count = 0;
 bool XYdicration = true; //true for x og false for y
@@ -29,28 +29,28 @@ void setup() {
 
 void loop() {
   distCal(xdist);
+  delay(1000);
+  distCal(xdist);
 
 }
 
 
 void distCal(double dist) {
   if (XYdicration == true) {
-    
+
     count = (((dist*0.785) * 900) / (PI * 3, 9)); // 0,785 er en værdi der ganges med fordi det virker
 
     
     Serial.println((String)count);
     Serial.println("Encoder1 count = " + (String)EncoderCount);
-    if (EncoderCount < count) {
+    while(EncoderCount < count) {
       
       getEncoder();    
-
-      motors.setSpeeds(105, 100);
+      motors.setSpeeds(105,100);
     }
-    else {
       Serial.println("Kør");
       motors.setSpeeds(0, 0);
-    }
+    
 
   }
 
