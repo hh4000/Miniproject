df05cc91aacf756d989728f4d1cde4eaafa56236
Hans Henrik Dalgaard <hans.henrik.dalgaard@gmail.com>
2021-11-16 14:02:46 +0100

    changed function name to driveDist

--- Stage4Part3/XDrive.ino
+++ Stage4Part3/XDrive.ino
@@ -4,7 +4,7 @@
 
 double count = 0;
 
-void distCal(double dist) {
+void driveDist (double dist) {
   //resets encoders to avoid encoder counts for e.g. turns
   resetEncoders();
   //factor so that it can drive backwards
@@ -12,21 +12,23 @@ void distCal(double dist) {
   if (dist < 0) f = -1;
     
     count = dist*78.5; //Don't ask, it just works
+    
     //Following code prints on the lcd, the distance it is driving in cm
     lcd.gotoXY(0,0);
     lcd.print("Driving");
     lcd.gotoXY(0,1);
     lcd.print((String)dist + " cm");
-    
+   
     //actual function
-    while(f*encoders.getCountsRight()< f*count) {   
+    while(f*encoders.getCountsRight()< f*count) {
+      
       motors.setSpeeds(f*100,f*100);//standard speed
       //if loop so that it avoids driving unevenly 
-      if(encoders.getCountsRight()*1.01 > encoders.getCountsLeft()){
+      if(f*encoders.getCountsRight()*1.01 > f*encoders.getCountsLeft()){
         // when right counter is higher, the left motor goes to 300 speed for a moment to even out
         motors.setSpeeds(f*300,f*100); 
 
-      } else if (encoders.getCountsRight()*1.01 < encoders.getCountsLeft()){
+      } else if (f*encoders.getCountsRight()*1.01 < f*encoders.getCountsLeft()){
         //Same as above but with other motor
         motors.setSpeeds(f*100,f*300);
       }
